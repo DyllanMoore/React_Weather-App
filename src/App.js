55bdc7fe794299364af973dc4e5082164fb186d4
Dyllan Moore <dyllantmoore@gmail.com>
2022-06-07 16:33:56 -0500

    starting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Search">
+        <input type="text" />
+        <button type="submit">Search</button>
+      </div>
+      <div className="Body">
+        <h1>Location</h1>
+        <h2 className="WeatehrData">Temperature: </h2>
+        <h2 className="WeatehrData">Wind: </h2>
+        <h2 className="WeatehrData">Cloud Cover: </h2>
+        <h2 className="WeatehrData">Percipitation: </h2>
+      </div>
     </div>
   );
+}
+
+function GetLocation(props) {
+
+
 }
 
 export default App;
